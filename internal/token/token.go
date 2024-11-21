11e2b54ae8178a48c36717ce411f79cb04892f24
Kerem37mert <kerem37mert@yahoo.com>
2024-11-21 18:04:11 +0300

    makeToken created

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,8 +1,23 @@
 package token
 
+// The Token struct
 type Token struct {
+	tokenType   TokenType
 	value       string
 	fileName    string
 	column, row uint32
 	next        *Token
+}
+
+// Create a token and return this token
+func makeToken(tokenType TokenType, value, fileName string, column, row uint32) *Token {
+	var token *Token = new(Token)
+	token.tokenType = tokenType
+	token.value = value
+	token.fileName = fileName
+	token.column = column
+	token.row = row
+	token.next = nil
+
+	return token
 }
